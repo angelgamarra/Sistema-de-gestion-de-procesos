a4ac90183f3b9d70f1671ba4c234e0ff753d5883
angelgamarra <angelgamarra.1215588@gmail.com>
2025-06-08 23:21:48 -0500

    Pila de memoria

--- SGDP.cpp
+++ SGDP.cpp
@@ -8,3 +8,69 @@
 #include <string>
 
 using namespace std; // para evitar usar std:: en cada instruccion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// estructura para nodo de pila de memoria
+struct NodoMemoria {
+    int bloque_id; // identificador del bloque
+    NodoMemoria* sig; // siguiente nodo (bloque siguiente)
+};
+
+// clase pila para manejar los bloques de memoria asignados
+class PilaMemoria {
+private:
+    NodoMemoria* cima; // tope de la pila
+    int contador = 0; // contador para asignar id a cada bloque
+
+public:
+    // constructor: pila vacia
+    PilaMemoria() : cima(nullptr) {}
+
+    // asigna un nuevo bloque de memoria (push)
+    void push() {
+        NodoMemoria* nuevo = new NodoMemoria{++contador, cima}; // nuevo nodo
+        cima = nuevo; // actualiza cima
+    }
+
+    // libera el ultimo bloque asignado (pop)
+    void pop() {
+        if (!cima) return; // si esta vacia
+        NodoMemoria* temp = cima;
+        cima = cima->sig; // avanza la cima
+        delete temp; // libera memoria
+    }
+
+    // muestra los bloques actuales en la pila
+    void mostrar() {
+        NodoMemoria* actual = cima;
+        while (actual) {
+            cout << "bloque de memoria id: " << actual->bloque_id << endl;
+            actual = actual->sig;
+        }
+    }
+};
+
+
+
