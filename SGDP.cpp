6b9af932f862784609083486d2be21609248498b
angelgamarra <angelgamarra.1215588@gmail.com>
2025-06-08 23:07:02 -0500

    Comentarios iniciales

--- SGDP.cpp
+++ SGDP.cpp
@@ -1,4 +1,10 @@
+// sistema de gestion de procesos en c++ usando estructuras dinamicas lineales
+// se utiliza lista enlazada, cola de prioridad y pila
+// incluye persistencia de datos para los procesos
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
+using namespace std; // para evitar usar std:: en cada instruccion
